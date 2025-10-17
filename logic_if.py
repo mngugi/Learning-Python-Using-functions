39d6e304fd1bdd03d94b75933d35011250a15efa
P.M.Ngugi <mngugi7@hotmail.com>
2025-10-17 17:57:42 +0300

    Update logic_if.py

--- logic_if.py
+++ logic_if.py
@@ -6,4 +6,17 @@ def logic_if_loop():
         
 logic_if_loop()
 
-        
+ 
+ # -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Author : Mwangi
+Purpose : Simple hello world function
+programming starts at function modules
+"""
+def Hello():
+    p = ("Hello World Coders")
+    return p
+    
+# call the function hello()    
+print("Call the phrase in the function:", Hello())       
